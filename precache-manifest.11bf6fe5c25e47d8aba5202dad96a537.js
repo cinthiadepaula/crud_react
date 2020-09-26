33f00d57ea649349ba3e9f928d8ea9501c529696
cinthiadepaula <cinthiade.paula@outlook.com>
2020-09-26 16:53:44 -0300

    Updates

---
+++ precache-manifest.11bf6fe5c25e47d8aba5202dad96a537.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5667df539d5a9bb1b0b71de36805dce6",
+    "url": "/crud_react/index.html"
+  },
+  {
+    "revision": "087c7cdca55a66c42128",
+    "url": "/crud_react/static/css/2.fb7cd391.chunk.css"
+  },
+  {
+    "revision": "d2bd0a23ae496f3b549b",
+    "url": "/crud_react/static/css/main.06eab811.chunk.css"
+  },
+  {
+    "revision": "087c7cdca55a66c42128",
+    "url": "/crud_react/static/js/2.fd874ba1.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/crud_react/static/js/2.fd874ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bd0a23ae496f3b549b",
+    "url": "/crud_react/static/js/main.a20f1416.chunk.js"
+  },
+  {
+    "revision": "55115b1e567db01b0dda",
+    "url": "/crud_react/static/js/runtime-main.a01b8a13.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/crud_react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/crud_react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/crud_react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/crud_react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/crud_react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "26254cdb194f2d2a21efbe9f9165b04d",
+    "url": "/crud_react/static/media/logotipo.26254cdb.png"
+  }
+]);
